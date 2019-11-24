db6178c2e8e9f948697ad214a01562b597d2c13b
Mark Rawls <markrawls96@gmail.com>
2019-11-24 03:28:15 -0500

    Use Argon2 for password hashing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -155,6 +155,14 @@ REST_FRAMEWORK = {
 }
 
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
+
 # CORS
 
 CORS_ORIGIN_ALLOW_ALL = DEBUG
