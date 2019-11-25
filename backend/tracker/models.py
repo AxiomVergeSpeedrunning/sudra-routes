decb74088d05b05e35a8a089e8146c4930daf9dd
Mark Rawls <markrawls96@gmail.com>
2019-11-24 20:44:10 -0500

    Allow null/blank on initial field

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -5,4 +5,4 @@ from django.contrib.postgres.fields import HStoreField
 
 class TrackerInformation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='tracker_info')
-    data = HStoreField()
+    data = HStoreField(null=True, blank=True)
