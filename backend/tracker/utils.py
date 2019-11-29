80c82cb6b411c995b4358ffb979eeedd89ed51d6
Mark Rawls <markrawls96@gmail.com>
2019-11-28 19:21:56 -0500

    Add dict functionality

--- backend/tracker/utils.py
+++ backend/tracker/utils.py
@@ -2,24 +2,43 @@ import json
 from .translations import translations
 
 
-def generate_tree(data, instance, translator):
+def get(instance, attr, is_dict=False):
+    if is_dict:
+        try:
+            return instance[attr]
+        except KeyError:
+            instance[attr] = {}
+            return instance[attr]
+
+    return getattr(instance, attr)
+
+
+def set(instance, attr, val, is_dict=False):
+    if is_dict:
+        instance[attr] = val
+    else:
+        setattr(instance, attr, val)
+
+
+def generate_tree(data, instance, translator, is_dict=False):
     for translation in translator:
         if len(translation) == 3:
             orig, new, translator = translation
 
-            try:
-                getattr(instance, new)
-            except AttributeError:
-                setattr(instance, new, {})
+            if is_dict:
+                try:
+                    get(instance, new, is_dict)
+                except AttributeError:
+                    set(instance, new, {}, is_dict)
 
-            generate_tree(data.get(orig), getattr(instance, new), translator)
+            generate_tree(data.get(orig), get(instance, new, is_dict), translator)
             continue
 
         orig, new = translation
 
-        setattr(instance, new, data.get(orig))
+        set(instance, new, data.get(orig), is_dict)
 
 
-def translate_data(data, tracker_info):
+def translate_data(data, tracker_info, is_dict=False):
     parsed = json.loads(data)
-    generate_tree(parsed, tracker_info, translations)
+    generate_tree(parsed, tracker_info, translations, is_dict)
