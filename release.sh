7d797cdc341c34bc12c2deb20ca606847947448d
Mark Rawls <markrawls96@gmail.com>
2019-12-09 18:33:41 -0500

    Add logging to the deployment script

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 export OWD=$PWD
 
+echo "Uploading downloads to the public download CDN"
 cd frontend/static/downloads
 aws s3 sync --acl "public-read" . s3://com.sudra-routes.downloads/
 
@@ -14,5 +15,6 @@ source venv/bin/activate
 sudo find . -type d -name '__pycache__' -exec rm -rf '{}' ';' || true
 sudo find . -type f -iname '*.pyc' -exec rm '{}' ';' || true
 
+echo "Deploying backend to Lambda"
 zappa update production
 zappa manage production migrate
