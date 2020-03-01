2efb41bd9e1a495d6f87c564d628a449c7474f4f
Mark Rawls <markrawls96@gmail.com>
2020-03-01 18:13:32 -0500

    Re-enable discord auth

--- backend/custom_auth/views.py
+++ backend/custom_auth/views.py
@@ -64,8 +64,7 @@ def rtmp_check(request):
     failure = Response({}, status=status.HTTP_404_NOT_FOUND)
 
     try:
-        user = User.objects.get(username=request.data['name'], auth_token__key=request.data['token'], is_staff=True)
-        return Response({})
+        user = User.objects.get(username=request.data['name'], auth_token__key=request.data['token'])
         custom_info = CustomUserInformation.objects.get(user=user)
 
         response = requests.get(
