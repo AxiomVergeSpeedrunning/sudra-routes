4b666497be3b26ab073177911d4e2ff332a6dc5e
Mark Rawls <markrawls96@gmail.com>
2020-03-01 02:59:08 -0500

    Make custom info one to one

--- backend/custom_auth/models.py
+++ backend/custom_auth/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 
 class CustomUserInformation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='custom_info')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='custom_info')
     discord_user_id = models.CharField(max_length=256, null=True, blank=True)
