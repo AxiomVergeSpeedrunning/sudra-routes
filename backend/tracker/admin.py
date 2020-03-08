f54246d98b782720089a38aa73ed4349d4d227a9
Mark Rawls <markrawls96@gmail.com>
2020-03-08 19:50:34 -0400

    Register models with the admin site

--- backend/tracker/admin.py
+++ backend/tracker/admin.py
@@ -1,3 +1,6 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import TrackerInformation, ItemTracker
 
-# Register your models here.
+
+admin.site.register(TrackerInformation)
+admin.register(ItemTracker)
