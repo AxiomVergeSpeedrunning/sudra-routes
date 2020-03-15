26cba9415165b48ea03de9e4d011306c0b0095e4
Christopher D Couture <videogameroulette@yahoo.ca>
2020-03-14 22:42:11 -0400

    Theme Window Update 2
    added id fallthru

--- frontend/src/pages/Races/Admin/index.jsx
+++ frontend/src/pages/Races/Admin/index.jsx
@@ -1,81 +1,163 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { observer } from 'mobx-react';
-import gql from 'graphql-tag';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Typography from '@material-ui/core/Typography';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import { useSnackbar } from 'notistack';
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import classNames from 'classnames';
+import UrlSearchParams from '@ungap/url-search-params';
 
-import urls from 'urls';
-import { useGlobalContext } from 'hooks';
+import NormalBorder from '../assets/axiom-border.png';
+import PurpleBorder from '../assets/purple-border.png';
+import DeepRedBorder from '../assets/deep-red-border.png';
+import PinkBorder from '../assets/pink-border.png';
 
-import FixedFab from 'components/FixedFab';
-import AddIcon from '@material-ui/icons/Add';
+import GDQBorder from '../assets/gamer-tags-border.png';
+import GDQSubBorder from '../assets/gdq-frame.png';
 
-const ALL_RACES = gql`
-  {
-    races {
-      id
-      started
-      game_name
-      commentator_name
-      extra_information
+import GDQBorder2 from '../assets/gamer-tags-border-3.png';
+import GDQSubBorder2 from '../assets/gdq-frame2.png';
 
-      runners {
-        id
-        name
-        end_time
-      }
-    }
-  }
-`;
+import GDQBorder3 from '../assets/gamer-tags-border-4.png';
+import GDQSubBorder3 from '../assets/gdq-frame3.png';
 
-const CREATE_RACE = gql`
-  mutation CreateRace($input: CreateRaceInput!) {
-    createRace(input: $input) {
-      race {
-        id
-      }
-    }
-  }
-`;
+import AVSRBorder from '../assets/gamer-tags-border-2.png';
+import AVSRSubBorder from '../assets/avsr-frame.png';
 
-const AdminIndex = () => {
-  const { enqueueSnackbar } = useSnackbar();
-  const history = useHistory();
-  const { loading: userLoading } = useGlobalContext();
-  const { loading: racesLoading, data, refetch } = useQuery(ALL_RACES);
-  const [newRace] = useMutation(CREATE_RACE, {
-    onCompleted: mutationData => history.push(urls.races.admin.view + `${mutationData.race.id}/`),
-  });
+const useStyles = makeStyles({
+  gameWindow: {
+    backgroundColor: 'none',
+  },
+});
 
-  if (userLoading || racesLoading) {
-    return <LinearProgress />;
-  }
+const useStyles = makeStyles(theme => ({
+  // Used to modify other classes
+  purple: {},
+  deepRed: {},
+  pink: {},
+  slim: {},
+  gdq: {},
+  gdq2: {},
+  gdq3: {},
+  avsr: {},
+
+  root: {
+    padding: theme.spacing(3),
+    border: '10px solid transparent',
+    borderImage: `url(${NormalBorder}) 10 round`,
+    borderRadius: 12,
+    backgroundColor: '#130612',
+
+    '&$gdq': {
+      borderImage: `url(${GDQSubBorder}) 2 round`,
+      border: '2px solid transparent',
+      backgroundColor: '#021111',
+    },
+
+    '&$gdq2': {
+      borderImage: `url(${GDQSubBorder2}) 2 round`,
+      border: '2px solid transparent',
+      backgroundColor: '#021111',
+    },
+
+    '&$gdq3': {
+      borderImage: `url(${GDQSubBorder3}) 2 round`,
+      border: '2px solid transparent',
+      backgroundColor: '#021111',
+    },
+
+    '&$avsr': {
+      borderImage: `url(${AVSRSubBorder}) 2 round`,
+      border: '2px solid transparent',
+      backgroundColor: '#110209',
+    },
+
+    '&$purple': {
+      borderImageSource: `url(${PurpleBorder})`,
+      backgroundColor: '#000000',
+
+      '&$gdq': {
+        borderImage: `url(${GDQBorder}) 9 round`,
+        border: '9px solid transparent',
+      },
+      '&$gdq2': {
+        borderImage: `url(${GDQBorder2}) 9 round`,
+        border: '9px solid transparent',
+      },
+      '&$gdq3': {
+        borderImage: `url(${GDQBorder3}) 9 round`,
+        border: '9px solid transparent',
+      },
+      '&$avsr': {
+        borderImage: `url(${AVSRBorder}) 9 round`,
+        border: '9px solid transparent',
+      },
+    },
+
+    '&$deepRed': {
+      borderImageSource: `url(${DeepRedBorder})`,
+
+      '&$gdq': {
+        borderImage: `url(${GDQSubBorder}) 2 round`,
+      },
+      '&$gdq2': {
+        borderImage: `url(${GDQSubBorder2}) 2 round`,
+      },
+      '&$gdq3': {
+        borderImage: `url(${GDQSubBorder3}) 2 round`,
+      },
+      '&$avsr': {
+        borderImage: `url(${AVSRSubBorder}) 2 round`,
+      },
+    },
+
+    '&$pink': {
+      borderImageSource: `url(${PinkBorder})`,
+    },
+
+    '&$slim': {
+      padding: 4,
+    },
+  },
+}));
+
+const ThemedWindow = ({ children, variant, slim, className: externClassName, ...props }) => {
+  const location = useLocation();
+  const search = new UrlSearchParams(location.search);
+
+  const classes = useStyles();
+  const className = classNames(
+    classes.root,
+    {
+      [classes.purple]: variant === 'purple',
+      [classes.deepRed]: variant === 'red',
+      [classes.pink]: variant === 'pink',
+      [classes.slim]: slim,
+      [classes.gdq]: search.has('gdq') || variant === 'gdq',
+      [classes.gdq2]: search.has('gdq2') || variant === 'gdq2',
+      [classes.gdq3]: search.has('gdq3') || variant === 'gdq3',
+      [classes.avsr]: search.has('avsr') || variant === 'avsr',
+    },
+    externClassName,
+  );
 
   return (
-    <>
-      {data.races.map((race, idx) => (
-        <div key={idx}>
-          <Typography>{race.started}</Typography>
-          <Typography>{race.commentator_name}</Typography>
-          <Typography>{race.game_name}</Typography>
-          <Typography>{race.extra_information}</Typography>
-          {race.runners.map((runner, ridx) => (
-          <div key={ridx}>
-            <Typography>{runner.id}</Typography>
-            <Typography>{runner.name}</Typography>
-            <Typography>{runner.end_time}</Typography>
-          </div>
-          ))}
-        </div>
-      ))}
-      <FixedFab color="primary" onClick={newRace}>
-      <AddIcon />
-      </FixedFab>
-    </>
+    <Paper className={className} {...props}>
+      {children}
+    </Paper>
   );
 };
 
-export default observer(AdminIndex);
+ThemedWindow.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['default', 'red', 'purple', 'pink', 'gdq', 'gdq2', 'gdq3', 'avsr']),
+  slim: PropTypes.bool,
+};
+
+ThemedWindow.defaultProps = {
+  className: '',
+  variant: 'default',
+  slim: false,
+};
+
+export default ThemedWindow;
